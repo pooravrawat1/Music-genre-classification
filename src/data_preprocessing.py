27939eb58f4e147e6afd86c13b85a89b72370fef
Poorav <pooravrawat@gmail.com>
2025-09-03 00:26:45 -0400

    Fixed syntax

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -20,8 +20,8 @@ def preprocess():
     x_train, x_test, y_train, y_test = train_test_split(x_scaled, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded)
 
     os.makedirs("data/processed", exist_ok=True)
-    pd.DataFrame(X_train).to_csv("data/processed/X_train.csv", index=False)
-    pd.DataFrame(X_test).to_csv("data/processed/X_test.csv", index=False)
+    pd.DataFrame(x_train).to_csv("data/processed/X_train.csv", index=False)
+    pd.DataFrame(x_test).to_csv("data/processed/X_test.csv", index=False)
     pd.DataFrame(y_train).to_csv("data/processed/y_train.csv", index=False)
     pd.DataFrame(y_test).to_csv("data/processed/y_test.csv", index=False)
 
